a67bac281c585f34f7922bf73e97dd6f0d8a4c05
tkronmul <tim.kronmueller@yahoo.de>
2025-09-14 11:39:27 +0200

    Update saveForm.js
    Minimalbeispiel

--- api/saveForm.js
+++ api/saveForm.js
@@ -1,25 +1,19 @@
 export default async function handler(req, res) {
-      if (req.method === 'POST') {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-            res.setHeader('Access-Control-Allow-Heaaders', 'Content-Type');
-            if (req-method === 'OPTIONS') return res.status(200).end();
-
-            if (req.ethod !== 'POST') {
-                  return res.status(405).json({ message: 'Method &{req.method} not allowed' });
-            }
-            
-            try {
-                  // Daten speichern
-                  let data = req.body;
-                  if (typeof data === 'string') {
-                        data = JSON.parse(data);
-                  }
-                  console.log('Formulardaten:', data);
-                  return res.status(200).json({ message: 'Erfolgreich gespeichert' , received: data});
-            } catch (error) {
-                  console.error('Feler in saveForm:', error)
-                  return res.status(500).json({message: 'Fehler beim Verarbeiten der Daten', error: error.message })
-            }
-      } 
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+      res.setHeader('Access-Control-Allow-Heaaders', 'Content-Type');
+      if (req-method === 'OPTIONS') return res.status(200).end();
+      if (req.ethod !== 'POST') {
+            return res.status(405).json({ message: 'Method &{req.method} not allowed' });
+      }
+      try {
+            // Daten speichern
+            let data = req.body;
+            if (typeof data === 'string') data = JSON.parse(data);
+            console.log('Formulardaten:', data);
+            return res.status(200).json({ message: 'Erfolgreich gespeichert' , received: data});
+      } catch (error) {
+            console.error('Feler in saveForm:', error);
+            return res.status(500).json({message: 'Fehler beim Verarbeiten der Daten', error: error.message })
+      }
       }

8a9edf1c2824a3c41eb47f43ca5a73154cf9f1d5
tkronmul <tim.kronmueller@yahoo.de>
2025-09-14 11:12:23 +0200

    Update saveForm.js
    Anpassung

--- api/saveForm.js
+++ api/saveForm.js
@@ -1,7 +1,13 @@
 export default async function handler(req, res) {
       if (req.method === 'POST') {
-            res.setHeader('Allow', ['POST']);
-            return res.status(405).json({ message: 'Method &{req.method} not allowed' });
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+            res.setHeader('Access-Control-Allow-Heaaders', 'Content-Type');
+            if (req-method === 'OPTIONS') return res.status(200).end();
+
+            if (req.ethod !== 'POST') {
+                  return res.status(405).json({ message: 'Method &{req.method} not allowed' });
+            }
             
             try {
                   // Daten speichern

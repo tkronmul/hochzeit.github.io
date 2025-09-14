1e47dc22c02069b22b89134da28e25a08b96db80
tkronmul <tim.kronmueller@yahoo.de>
2025-09-14 10:23:54 +0200

    Update saveForm.js
    anpassen

--- api/saveForm.js
+++ api/saveForm.js
@@ -1,8 +1,15 @@
 export default async function handler(req, res) {
       if (req.method === 'POST') {
-        // Daten speichern
-        res.status(200).json({ message: 'Erfolgreich gespeichert' });
+            try {
+                  // Daten speichern
+                  const data = req.body;
+                  console.log('Formulardaten:', data);
+                  res.status(200).json({ message: 'Erfolgreich gespeichert' });
+            } catch (error) {
+                  res.status(500).json({message: 'Fehler beim Verarbeiten der Daten', error: error.message })
+            }
       } else {
-        res.status(405).json({ message: 'Method not allowed' });
+            res.seHeader('Allow', ['POST']);
+      res.status(405).json({ message: 'Method not allowed' });
       }
       }

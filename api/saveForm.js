e53421262d1f7aa8679743be84da3688bbde0acf
tkronmul <tim.kronmueller@yahoo.de>
2025-09-14 10:10:16 +0200

    Update saveForm.js
    fix

--- api/saveForm.js
+++ api/saveForm.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel API!" });
-}
+export default async function handler(req, res) {
+      if (req.method === 'POST') {
+        // Daten speichern
+        res.status(200).json({ message: 'Erfolgreich gespeichert' });
+      } else {
+        res.status(405).json({ message: 'Method not allowed' });
+      }
+      }
